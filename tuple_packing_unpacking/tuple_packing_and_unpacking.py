b46ca0f2559a0a539230ab12e1d1cfb914d3ad01
Ali Osman Zengin <aliosmanzengin0212@gmail.com>
2023-07-04 10:57:15 +0200

    tuple glossary

--- tuple_packing_unpacking/tuple_packing_and_unpacking.py
+++ tuple_packing_unpacking/tuple_packing_and_unpacking.py
@@ -119,3 +119,17 @@ lst_tups = [('Articuno', 'Moltres', 'Zaptos'), ('Beedrill', 'Metapod', 'Charizar
 t_check = [tup[2] for tup in lst_tups if len(tup) >= 3]
 
 print(t_check)
+
+"""Glossary
+tuple
+A type of sequence, much like a list but immutable. A tuple is created by enclosing one or more values in parentheses, separated by commas.
+
+packing
+When multiple values are specified, separated by commas, they are packed into a tuple.
+
+unpacking
+When a tuple is assigned to a collection of variable names separated by commas, the tuple is unpacked and the separate values are assigned to each of the variables.
+
+pair
+A tuple with exactly two items.
+"""
